0af691113f73c37d73ca7bb38ffff2be52cae8c6
Erik Baauw <ebaauw@users.noreply.github.com>
2017-03-12 23:26:23 +0100

    Initial upload

---
+++ json.sh
@@ -0,0 +1,382 @@
+#!/bin/bash
+
+# json.sh
+# Copyright © 2017 Erik Baauw. All rights reserved.
+
+# Bash library process Java Script Object Notation, JSON.
+# See http://json.org/ for definition of JSON.
+
+# Parse JSON.
+# Usage: json [-njuakvtl] [-p path] [-c string | file]
+function json() {
+  local -i nlines=0    # Number lines output.
+  local value=        # Last value parsed.
+  local nvalue=        # Last value parsed w/o white space.
+
+  # Parameters
+  local nflag=false
+  local jflag=false
+  local uflag=false
+  local aflag=false
+  local kflag=false
+  local vflag=false
+  local tflag=false
+  local lflag=false
+  local patharg=
+  local stringarg=
+  local filearg=
+
+  # Output configuration.
+  local nl        # Newline.
+  local sp        # Space.
+  local o_b       # Object begin.
+  local o_e       # Object end.
+  local k_b       # Key begin.
+  local k_v       # Key/value separator.
+  local k_e       # Key end.
+  local v_b       # Value begin.
+  local echo      # Command for default output.
+  local echoj     # Command for key/value output.
+
+  # Print usage message on stderr
+  # Usage: _json_usage
+  function _json_usage() {
+    cat >&2 <<+
+
+Usage: json [-njuakvtl] [-p path] [-c string | file]
+
+By default, json reads JSON from stdin, formats it, and echoes it to stdout.
+The following parameters modify this behaviour:
+  -n         Do not include spaces nor newlines in output.
+  -j         Output JSON array of objects for each key/value pair.
+             Each object contains two key/value pairs: key "key" with an array
+             of keys as value and key "value" with the value as value.
+  -u         Output JSON array of objects for each key/value pair.
+             Each object contains one key/value pair: the path (concatenated
+             keys separated by '/') as key and the value as value.
+  -a         Output path:value in plain text instead of JSON.
+  -k         Limit output to keys. With -u output JSON array of paths.
+  -v         Limit output to values. With -u output JSON array of values.
+  -t         Limit output to top-level key/values.
+  -l         Limit output to leaf (non-array, non-object) key/values.
+  -p path    Limit output to key/values under path. Set top level below path.
+  -c string  Read JSON from string instead of from stdin
+  file       Read JSON from file instead of from stdin
++
+  }
+
+  # Print error message on stderr.
+  # Usage: _json_error message
+  function _json_error() {
+    echo "json: error: ${1}" >&2
+  }
+
+  # Process command line parameters.
+  # Usage: _json_params [-njuakvtl] [-p path] [-c string | file]
+  function _json_params() {
+    local f
+
+    # Parse options.
+    unset OPTIND
+    while getopts :njuakvtlp:c: f ; do
+      case "${f}" in
+        n)  nflag=true
+            ;;
+        j)  jflag=true
+            ;;
+        u)  uflag=true
+            jflag=true
+            ;;
+        a)  aflag=true
+            uflag=true
+            jflag=true
+            nflag=true
+            ;;
+        k)  ${vflag} || kflag=true
+            jflag=true
+            ;;
+        v)  ${kflag} || vflag=true
+            jflag=true
+            ;;
+        t)  tflag=true
+            jflag=true
+            ;;
+        l)  lflag=true
+            jflag=true
+            ;;
+        p)  patharg="${OPTARG}"
+            ;;
+        c)  stringarg="${OPTARG}"
+            ;;
+        *)  _json_error "invalid option -${OPTARG}"
+            _json_usage
+            return 1
+            ;;
+      esac
+    done
+    shift $((${OPTIND} - 1))
+    [ $# -gt 1 ] && _json_usage && return 1
+    if [ $# -ge 1 ] ; then
+      [ ! -z "${stringarg}" ] && _json_usage && return 1
+      [ ! -r "${1}" ] && _json_error "${1}: cannot open" && return 1
+      filearg="${1}"
+    fi
+
+    # Configure output.
+    ${nflag} && nl= || nl=$'\n'
+    ${nflag} && sp= || sp=" "
+    o_b="${nl}${sp}${sp}{${sp}"
+    o_e="${sp}}"
+    if ${uflag} ; then
+      k_b="\""
+      k_e="\""
+      v_b=
+      k_v="\":${sp}"
+      if ${kflag} || ${vflag} ; then
+        o_b="${nl}${sp}${sp}"
+        o_e=
+      fi
+    else
+      k_b="\"key\":${sp}["
+      k_e="]"
+      v_b="\"value\":${sp}"
+      k_v="],${sp}\"value\":${sp}"
+    fi
+  }
+
+  # Break up input into tokens.
+  # Usage: _json_analyse
+  function _json_analyse() {
+    local -r spc='[[:space:]]'
+    local -r chr='[^[:cntrl:]"\\]'
+    local -r esc='\\["\\/bfnrt]|\\u[0-9a-fA-F]{4}'
+    local -r str="\"${chr}*(${esc}${chr}*)*\""
+    local -r num='-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][+-]?[0-9]+)?'
+    local -r wrd='[A-Za-z_][A-Za-z0-9_]*'
+
+    if [ ! -z "${filearg}" ] ; then
+      egrep -ao "${spc}|${str}|${num}|${wrd}|." <"${filearg}"
+    elif [ ! -z "${stringarg}" ] ; then
+      egrep -ao "${spc}|${str}|${num}|${wrd}|." <<<"${stringarg}"
+    else
+      egrep -ao "${spc}|${str}|${num}|${wrd}|."
+    fi | egrep -v "^${spc}$"
+  }
+
+  # Parse JSON array.
+  # Usage: _json_array path keylist indent depth
+  function _json_array () {
+    local path="${1}"
+    local keylist="${2}"
+    local indent="${3}"
+    local -i depth=${4}
+    local t           # Token.
+    local -i n=0      # Number of elements.
+    local v="["       # Array's value.
+    local nv="["      # Array's value w/o white space.
+
+    read -r t                                                       # matching [
+    while [ "${t}" != "]" ] ; do
+      if [ ${n} -gt 0 ] ; then
+        if [ "${t}" != "," ] ; then                                 # matching [
+          _json_error "expected ',' or ']', got '${t}'"
+          return 1
+        fi
+        v="${v},"
+        nv="${nv},"
+        read -r t
+      fi
+      _json_value "${t}" "${path:+${path}/}${n}" \
+                  "${keylist:+${keylist},${sp}}${n}" \
+                  "${indent}${sp}${sp}" \
+                  $((${depth} + 1)) || return 1
+      v="${v}${nl}${indent}${sp}${sp}${value}"
+      nv="${nv}${nvalue}"
+      n="n + 1"
+      read -r t
+    done
+    [ ${n} -gt 0 ] && v="${v}${nl}${indent}"
+    value="${v}]"
+    nvalue="${nv}]"
+  }
+
+  # Parse JSON object.
+  # Usage: _json_object path keylist indent depth
+  function _json_object() {
+    local path="${1}"
+    local keylist="${2}"
+    local indent="${3}"
+    local -i depth=${4}
+    local t           # Token.
+    local -i n=0      # Number of key/value pairs.
+    local v="{"       # Object's value.
+    local nv="{"      # Object's value w/o white space.
+    local k           # Key element.
+    local p           # Path element.
+
+    read -r t                                                       # matching {
+    while [ "${t}" != "}" ] ; do
+      if [ ${n} -eq 0 ] ; then
+        if [[ "${t}" != '"'*'"' ]] ; then                           # matching {
+          _json_error "expected '\"' or '}', got '${t}'"
+          return 1
+        fi
+      else
+        if [ "${t}" != "," ] ; then    # matching {
+          _json_error "expected ',' or '}', got '${t}'"
+          return 1
+        fi
+        v="${v},"
+        nv="${nv},"
+        read -r t
+        if [[ "${t}" != '"'*'"' ]] ; then
+          _json_error "expected '\"', got '${t}'"
+          return 1
+        fi
+      fi
+      k="${t}"
+      p="$(eval echo -n ${t})"
+      read -r t
+      if [ "${t}" != ":" ] ; then
+        _json_error "expected ':', got '${t}'"
+        return 1
+      fi
+      read -r t
+      _json_value "${t}" "${path:+${path}/}${p}" \
+                  "${keylist:+${keylist},${sp}}${k}" \
+                  "${indent}${sp}${sp}" \
+                  $((${depth} + 1)) || return 1
+      v="${v}${nl}${indent}${sp}${sp}${k}:${sp}${value}"
+      nv="${nv}${k}:${nvalue}"
+      n="n + 1"
+      read -r t
+    done
+    [ ${n} -gt 0 ] && v="${v}${nl}${indent}"
+    value="${v}}"
+    nvalue="${nv}}"
+  }
+
+  # Parse JSON value.
+  # Usage: _json_value token path keylist indent depth
+  # Precondition: token is (first token of) value
+  function _json_value() {
+    local t="${1}"    # Token
+    local path="${2}"
+    local keylist="${3}"
+    local indent="${4}"
+    local -i depth=${5}
+    local isleaf=false
+    local saved_patharg=
+    local k
+
+    if [ ! -z "${patharg}" -a "${path}" = "${patharg}" ] ; then
+      # Set top level for -p path.
+      saved_patharg="${patharg}"
+      path=
+      keylist=
+      indent=
+      depth=0
+      patharg=
+    fi
+    if [ -z "${patharg}" -a ${depth} -eq 0 ] ; then
+      # Top level.
+      if ${jflag} ; then
+        ${aflag} || echo -n "["
+        indent="${sp}${sp}"
+      fi
+    fi
+    case "${t}" in
+      '"'*'"'|[0-9-]*|true|false|null)
+        isleaf=true
+        value="${t}"
+        nvalue="${t}"
+        ;;
+      '{')                                                          # matching }
+        _json_object "${path}" "${keylist}" "${indent}" \
+              "${depth}" || return 1
+        ;;
+      '[')                                                          # matching ]
+        _json_array "${path}" "${keylist}" "${indent}" \
+              "${depth}" || return 1
+        ;;
+      '"')  read -r t
+        _json_error "invalid string '\"${t}'"
+        return 1
+        ;;
+      *)  _json_error "expected value, got '${t}'"
+        return 1
+        ;;
+    esac
+    if ${jflag} ; then
+      ${uflag} && k="${path}" || k="${keylist}"
+      if [[ "${path}" != "${patharg}"* ]] ; then
+        # No match for -p path.
+        :
+      elif ${tflag} && [ ${depth} -ne 1 ] ; then
+        # Not top level and -t.
+        :
+      elif ${lflag} && [ ${isleaf} = false ] ; then
+        # Not leaf and -l.
+        :
+      elif ${aflag} ; then
+        # Output line for -a.
+        if [ -z "${patharg}" -a ${depth} -eq 0 -a \
+              ${isleaf} = false ] ; then
+          :
+        elif ${kflag} ; then
+          [ ! -z "${k}" ] && echo "${k}"
+        elif ${vflag} ; then
+          echo "${nvalue}"
+        else
+          [ -z "${k}" ] && echo "${nvalue}" || \
+              echo "${k}:${nvalue}"
+        fi
+      else
+        # Output line for -j, -u.
+        [ ${nlines} -gt 0 ] && echo -n ","
+        if ${kflag} ; then
+          echo -n "${o_b}${k_b}${k}${k_e}${o_e}"
+        elif ${vflag} ; then
+          echo -n "${o_b}${v_b}${nvalue}${o_e}"
+        else
+          echo -n \
+          "${o_b}${k_b}${k}${k_v}${nvalue}${o_e}"
+        fi
+        nlines="nlines + 1"
+      fi
+    fi
+    if [ -z "${patharg}" -a ${depth} -eq 0 ] ; then
+      # Top level.
+      if ${jflag} ; then
+        if ${aflag} ; then
+          :
+        else
+          [ ${nlines} -gt 0 ] && echo
+          echo "]"
+        fi
+      else
+        echo "${value}"
+      fi
+    fi
+    if [ ! -z "${saved_patharg}" ] ; then
+      # Unset top level for -p path.
+      patharg="${saved_patharg}"
+    fi
+  }
+
+  # Usage: _json_parse
+  function _json_parse() {
+    local t
+
+    read -r t
+    _json_value "${t}" "" "" "" 0 || return 1
+    read -r t
+    if [ "${t}" != "" ] ; then
+      _json_error "expected EOF, got '${t}'"
+      return 1
+    fi
+  }
+
+  _json_params "${@}" || return 1
+  _json_analyse | _json_parse
+}
---
+++ ph.sh
@@ -0,0 +1,309 @@
+#!/bin/bash
+#
+# ph.sh
+# Copyright © 2017 Erik Baauw. All rights reserved.
+#
+# Shell library for interacting with the Philips Hue bridge using its REST API.
+# Except where noted, functions work on deCONZ as well.
+
+# ===== CONFIGURATION ==========================================================
+
+# Check whether json command is available, otherwise load json.sh.
+json -c 0 >/dev/null 2>&1
+[ $? -ne 0 ] && . json.sh
+
+# Check whether ph_host is set, otherwise use first bridge registered to
+# meethue portal.
+if [ -z "${ph_host}" ] ; then
+  ph_host=$(ph_unquote "$(ph_nupnp | json -avp /0/internalipaddress)")
+fi
+
+# Set default values.
+: ${ph_username:=empty}
+: ${ph_verbose:=false}
+: ${ph_debug:=false}
+
+# ===== BASIC FUNCTIONS ========================================================
+
+# Return value for path from Philips Hue bridge on standard output.
+# Path can be a full path, e.g. /lights/1/state/on, even when Philips Hue bridge
+# only supports GET on resources like /lights/1.
+# Usage: body=$(ph_get path)
+function ph_get() {
+  local resource="${1#/}"
+  local path=
+  local p="${resource%%/*}"
+  local response
+
+  case "${p}" in
+    lights|groups|schedules|scenes|sensors|rules|resourcelinks)
+      path="${resource#${p}}"
+      path="${path#/}"
+      resource="${p}"
+      p="${path%%/*}"
+      if [[ "${p}" == [A-Za-z0-9-]* ]] ; then
+        resource="${resource}/${p}"
+        path="${path#${p}}"
+        path="${path#/}"
+      fi
+      ;;
+    config|capabilities)
+      path="${resource#${p}}"
+      path="${path#/}"
+      resource="${p}"
+      ;;
+    *)
+      ;;
+  esac
+  response=$(_ph_http GET "/${resource}")
+  [ $? -ne 0 ] && return 1
+  if [ -z "${path}" ] ; then
+    json -c "${response}"
+    return 0
+  fi
+  response=$(json -c "${response}" -p "${path}")
+  if [ -z "${response}" ] ; then
+    echo "error: '/${path}' not found in resource '/${resource}'" >&2
+    return 1
+  fi
+  echo "${response}"
+}
+
+# Update resource on Philips Hue bridge.
+# Usage: ph_put resource body
+function ph_put() {
+  local response
+
+  response=$(_ph_http PUT "${1}" "${2}")
+  [ $? -eq 0 ] || return 1
+  if ${ph_verbose} ; then
+    json -c "${response}"
+  fi
+}
+
+# Create resource on the Philips Hue bridge.
+# Usage: id=$(ph_post resource body)
+function ph_post() {
+  local response
+
+  response=$(_ph_http POST "${1}" "${2}")
+  [ $? -eq 0 ] || return 1
+  if ${ph_verbose} ; then
+    json -c "${response}"
+  else
+    ph_unquote "$(json -al -c "${response}" | cut -f 2 -d :)"
+  fi
+}
+
+# Delete resource from the Philips Hue Bridge.
+# Usage: ph_delete resource
+function ph_delete() {
+  local response
+
+  response=$(_ph_http DELETE "${1}")
+  [ $? -eq 0 ] || return 1
+  if ${ph_verbose} ; then
+    json -c "${response}"
+  fi
+}
+
+# ===== LIGHT STATE ============================================================
+
+# Power-on color temperature.
+ph_ct_poweron=366       # 2,732 Kelvin
+
+# Light recipe color temperatures.
+ph_ct_relaxed=447       # 2,237 Kelvin
+ph_ct_read=346          # 2,890 Kelvin
+ph_ct_concentrate=233   # 4,292 Kelvin
+ph_ct_energize=156      # 6,410 Kelvin
+
+# ===== BRIDGE FUNCTIONS =======================================================
+
+# Press bridge link button (Hue bridge only).
+# Usage: ph_linkbutton
+function ph_linkbutton() {
+  ph_put /config '{"linkbutton": true}'
+}
+
+# Create username.
+# Usage ph_username
+function ph_createuser() {
+  local devicetype="ph.sh#$(hostname -s)"
+  ph_username= ph_post / "{\"devicetype\":\"${devicetype}\"}"
+}
+
+# Perform bridge touchlink (Hue bridge only).
+# Usage: ph_touchlink
+function ph_touchlink() {
+  ph_put /config '{"touchlink": true}'
+}
+
+# Reset bridge homekit status (Hue bridge v2 only).
+# Usage: ph_reset_homekit
+function ph_reset_homekit() {
+  ph_put /config '{"homekit":{"factoryreset": true}}'
+}
+
+# ===== BRIDGE DISCOVERY =======================================================
+
+# Find bridges using nupnp method (through the meethue portal).
+# Usage: ph_nupnp
+function ph_nupnp() {
+  local cmd
+  local response
+
+  cmd="curl -s \"https://www.meethue.com/api/nupnp\""
+  ${ph_debug} && echo "debug: meethue portal command: ${cmd}" >&2
+  response=$(eval ${cmd})
+  if [ $? -ne 0 ] ; then
+    echo "error: meethue portal not found" >&2
+    return 1
+  fi
+  ${ph_debug} && echo "debug: meethue portal response: ${response}" >&2
+  json -c "${response}"
+}
+
+# Find deCONZ using nupnp method (through the dresden elektronik portal).
+# Usage: ph_nupnp_deconz
+function ph_nupnp_deconz() {
+  local cmd
+  local response
+
+  cmd="curl -s \"https://dresden-light.appspot.com/discover\""
+  ${ph_debug} && echo "debug: deCONZ portal command: ${cmd}" >&2
+  response=$(eval ${cmd})
+  if [ $? -ne 0 ] ; then
+    echo "error: deCONZ portal not found" >&2
+    return 1
+  fi
+  ${ph_debug} && echo "debug: deCONZ portal response: ${response}" >&2
+  json -c "${response}"
+}
+
+# Show bridge UPnP description.
+# Usage: ph_description
+function ph_description {
+  local cmd
+  local response
+
+  cmd="curl -s \"http://${ph_host}/description.xml\""
+  ${ph_debug} && echo "debug: hue bridge command: ${cmd}" >&2
+  response=$(eval ${cmd})
+  if [ $? -ne 0 ] ; then
+    echo "error: hue bridge not found" >&2
+    return 1
+  fi
+  ${ph_debug} && echo "debug: hue bridge response: ${response}" >&2
+  echo "${response}"
+}
+
+# Unauthorised config (Hue bridge only).
+# Usage: ph_config
+function ph_config() {
+  ph_username= ph_get /config
+}
+
+# ===== UTILITY FUNCTIONS ======================================================
+
+# Issue HTTP command to Philips Hue bridge and return response.
+# Note response is returned only when bridge command succeeded.  Otherwise a
+# message is printed on standard error and a non-zero status is returned.
+# Usage: response=$(ph_http GET|PUT|POST|DELETE resource [body])
+function _ph_http() {
+  local resource
+  local method
+  local data
+  local cmd
+  local response
+  local responselines
+  local errorlines
+
+  # Check parameters
+  case "${1}" in
+    GET)
+      method=
+      data=
+      ;;
+    PUT|POST)
+      method=" -X ${1}"
+      if [ -z "${3}" ] ; then
+        data=
+      else
+        data=$(json -nc "${3}" 2>/dev/null)
+        if [[ $? -ne 0 || "${data}" != {*} ]] ; then
+          echo "error: invalid body '${3}'" >&2
+          return 1
+        fi
+        data=" -d '${3}'"
+      fi
+      ;;
+    DELETE)
+      method=" -X ${1}"
+      data=
+      ;;
+    *)
+      echo "error: invalid method '${1}'" >&2
+      return 1
+      ;;
+  esac
+  if [[ "${2}" != /* ]] ; then
+    echo "error: invalid resource '${2}'" >&2
+    return 1
+  fi
+  if [ "${ph_username}" == empty ] ; then
+    echo "error: ph_username not set" >&2
+    return 1
+  elif [ -z "${ph_username}" ] ; then
+    resource="/api${2}"
+  else
+    resource="/api/${ph_username}${2}"
+  fi
+  if [ -z "${ph_host}" ] ; then
+    echo "error: ph_host not set" >&2
+    return 1
+  fi
+
+  # Send HTTP request to the Hue bridge.
+  cmd="curl -s${method} -H \"Content-Type: application/json\"${data}"
+  cmd="${cmd} \"http://${ph_host}${resource}\""
+  ${ph_debug} && echo "debug: hue bridge command: ${cmd}" >&2
+  response=$(eval ${cmd})
+  if [ $? -ne 0 ] ; then
+    echo "error: hue bridge '${ph_host}' not found" >&2
+    return 1
+  fi
+  ${ph_debug} && echo "debug: hue bridge response: ${response}" >&2
+
+  # Check response for errors.
+  responselines=$(json -al -c "${response}" 2>/dev/null)
+  if [ $? -ne 0 -o "${response}" == '[]' ] ; then
+    echo "error: invalid method ${1} for resource ${2}" >&2
+    return 1
+  fi
+  errorlines=$(echo "${responselines}" | grep /error/type: | cut -f 2 -d /)
+  if [ ! -z "${errorlines}" ] ; then
+    for i in ${errorlines} ; do
+      local -i errno=$(json -avp /${i}/error/type -c "${response}")
+      local error=$(json -avp /${i}/error/description -c "${response}")
+      error=$(ph_unquote "${error}")
+      echo "error: hue bridge error ${errno}: ${error}" >&2
+    done
+    return 1
+  fi
+
+  # Output response.
+  echo "${response}"
+}
+
+# Return quoted string from string.
+# Usage: s=$(ph_quote string)
+function ph_quote() {
+  [[ "${1}" == '"'*'"' ]] && echo "${1}" || echo "\"${1}\""
+}
+
+# Remove quotes from string; return unquoted string from string.
+# Usage: s=$(ph_unquote string)
+function ph_unquote() {
+  [[ "${1}" == '"'*'"' ]] && eval echo "${1}" || echo "${1}"
+}
