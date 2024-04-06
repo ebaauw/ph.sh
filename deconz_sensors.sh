cd33e64296d49284d71f93ba3fd26ef241dfb929
Erik Baauw <erik.baauw@xs4all.nl>
2024-04-06 20:08:28 +0200

    Update deconz_sensors.sh

--- deconz_sensors.sh
+++ deconz_sensors.sh
@@ -21,7 +21,7 @@ fi
 # HomeKit does room assignment per accessory.
 # Provide an empty mid ("") to have homebridge-hue create a separate accessory
 # for the CLIP sensor resource.
-# Usage: id=$(_deconz_sensor_clip id name type [swversion])
+# Usage: id=$(_deconz_sensor_clip id name type uniqueid [swversion])
 function _deconz_sensor_clip()
 {
   local response
@@ -32,8 +32,8 @@ function _deconz_sensor_clip()
     \"type\": \"${3}\",
     \"modelid\": \"${3}\",
     \"manufacturername\": \"homebridge-deconz\",
-    \"swversion\": \"${4:-0}\",
-    \"uniqueid\": \"/sensors/${1}\"
+    \"swversion\": \"${5:-0}\",
+    \"uniqueid\": \"${4:-S${1}}\"
   }"))
   [ $? -ne 0 ] && return 1
   _deconz_info "/sensors/${id}: ${3} \"${2}\""
@@ -49,101 +49,57 @@ function deconz_sensor_clip_flag() {
   local version=1
 
   [ -z "${3}" ] || version=0
-  _deconz_sensor_clip "${1}" "${2}" CLIPGenericFlag "${version}"
+  _deconz_sensor_clip "${1}" "${2}" CLIPGenericFlag  "S${1}-01-0006" "${version}"
 }
 
 # Create CLIPGenericStatus sensor.
 # swversion is used to indicate to homebridge-deconz what the minimum and maximum
 # allowed values for the status are.
-# Usage: id=$(deconz_sensor_clip_status id name [min max]])
+# Usage: id=$(deconz_sensor_clip_status id name [aid] [min max]])
 function deconz_sensor_clip_status() {
-  _deconz_sensor_clip "${1}" "${2}" CLIPGenericStatus "${3:-0},${4:-2}"
+  _deconz_sensor_clip "${1}" "${2}" CLIPGenericStatus "${3:-${1}}-01-0012" "${4:-0},${5:-2}"
 }
 
 # Create CLIPPresence sensor.
-# Usage: id=$(deconz_sensor_clip_presence id name)
+# Usage: id=$(deconz_sensor_clip_presence id name [aid])
 function deconz_sensor_clip_presence() {
-  _deconz_sensor_clip "${1}" "${2}" CLIPPresence
+  _deconz_sensor_clip "${1}" "${2}" CLIPPresence "S${3:-${1}}-01-0406"
 }
 
 # Create CLIPLightLevel sensor.
-# Usage: id=$(deconz_sensor_clip_lightlevel id name [tholddark tholdoffset])
+# Usage: id=$(deconz_sensor_clip_lightlevel id name [aid] [tholddark tholdoffset])
 function deconz_sensor_clip_lightlevel() {
-  local -i id=$(_deconz_sensor_clip "${1}" "${2}" CLIPLightLevel)
+  local -i id=$(_deconz_sensor_clip "${1}" "${2}" CLIPLightLevel "S${3:-${1}}-01-0400")
   [ $? -eq 0 ] || return 1
   deconz put "/sensors/${id}/config" "{
-    \"tholddark\": ${3:-12000},
-    \"tholdoffset\": ${4:-4000}
+    \"tholddark\": ${4:-12000},
+    \"tholdoffset\": ${5:-4000}
   }"
   echo "${id}"
 }
 
 # Create CLIPTemperature sensor.
-# Usage: id=$(deconz_sensor_clip_temperature id name)
+# Usage: id=$(deconz_sensor_clip_temperature id name [aid])
 function deconz_sensor_clip_temperature() {
-  _deconz_sensor_clip "${1}" "${2}" CLIPTemperature
+  _deconz_sensor_clip "${1}" "${2}" CLIPTemperature "S${3:-${1}}-01-0402"
 }
 
 # Create CLIdeconzumidity sensor.
-# Usage: id=$(deconz_sensor_clip_humidity id name)
+# Usage: id=$(deconz_sensor_clip_humidity id name [aid])
 function deconz_sensor_clip_humidity() {
-  _deconz_sensor_clip "${1}" "${2}" CLIdeconzumidity
+  _deconz_sensor_clip "${1}" "${2}" CLIPhumidity "S${3:-${1}}-01-0405"
 }
 
 # Create CLIPPressure sensor.
-# Usage: id=$(deconz_sensor_clip_pressure id name)
+# Usage: id=$(deconz_sensor_clip_pressure id name [aid])
 function deconz_sensor_clip_pressure() {
-  _deconz_sensor_clip "${1}" "${2}" CLIPPressure
+  _deconz_sensor_clip "${1}" "${2}" CLIPPressure "S${3:-${1}}-01-0403"
 }
 
 # Create CLIPOpenClose sensor.
-# Usage: id=$(deconz_sensor_clip_openclose id name)
+# Usage: id=$(deconz_sensor_clip_openclose id name [aid])
 function deconz_sensor_clip_openclose() {
-  _deconz_sensor_clip "${1}" "${2}" CLIPOpenClose
-}
-
-# Create MultiCLIP resourcelink.
-# Usage: id=$(deconz_sensor_multiclip id [id...])
-function deconz_sensor_multiclip() {
-  deconz delete "/resourcelinks/${1}" >/dev/null 2>&1
-  local links="\"/sensors/${1}\""
-  local -i one="${1}"
-  shift
-  for i in "${@}" ; do
-    links="${links}, \"/sensors/${i}\""
-  done
-  local -i id=$(deconz_unquote $(deconz post "/resourcelinks" "{
-    \"name\": \"homebridge-hue\",
-    \"description\": \"multiclip\",
-    \"classid\": 1,
-    \"links\": [${links}]
-  }"))
-  [ $? -ne 0 ] && return 1
-  _deconz_info "/resourcelinks/${id}: multiclip"
-  [ ${id} -ne ${one} ] && _deconz_warn "/resourcelinks/${id}: not requested id ${one}"
-  echo ${id}
-}
-
-# Create MultiLight resourcelink.
-# Usage: id=$(deconz_light_multilight id [id...])
-function deconz_light_multilight() {
-  deconz delete "/resourcelinks/${1}" >/dev/null 2>&1
-  local links="\"/lights/${1}\""
-  local -i one="${1}"
-  shift
-  for i in "${@}" ; do
-    links="${links}, \"/lights/${i}\""
-  done
-  local -i id=$(deconz_unquote $(deconz post "/resourcelinks" "{
-    \"name\": \"homebridge-hue\",
-    \"description\": \"multilight\",
-    \"classid\": 1,
-    \"links\": [${links}]
-  }"))
-  [ $? -ne 0 ] && return 1
-  _deconz_info "/resourcelinks/${id}: multilight"
-  [ ${id} -ne ${one} ] && _deconz_warn "/resourcelinks/${id}: not requested id ${one}"
-  echo ${id}
+  _deconz_sensor_clip "${1}" "${2}" CLIPOpenClose "S${3:-${1}}-01-0500"
 }
 
 # ===== ZIGBEE SENSORS =========================================================
@@ -168,17 +124,8 @@ function deconz_sensor_presence() {
   deconz_sensor_name "${1}" "${2}"
   [ $? -ne 0 ] && return 1
   deconz put "/sensors/${1}/config" "{
-    \"sensitivity\": ${3:-2}
+    \"sensitivity\": ${3:-4}
   }"
-  [ $? -eq 0 ] || return 1
-  if [ "${_deconz_model}" != "deCONZ" ] ; then
-    id=$(deconz_unquote $(deconz post /resourcelinks "{
-      \"name\": \"${2}\",
-      \"classid\": 10010,
-      \"links\": [ \"/sensors/${1}\" ]
-    }"))
-    _deconz_info "/resourcelinks/${id}: ${2}"
-  fi
 }
 
 # Set Hue Motion lightlevel sensor name and thresholds.
@@ -197,7 +144,6 @@ function deconz_sensor_lightlevel() {
 function deconz_sensor_temperature() {
   deconz_sensor_name "${1}" "${2}"
   [ $? -eq 0 ] || return 1
-  [ "${_deconz_model}" == "deCONZ" ] || return 0
   deconz put "/sensors/${1}/config" "{
     \"offset\": ${3:-0}
   }"
